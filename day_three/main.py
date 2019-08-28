20b9f91db700a4ae44fb5ace9a17614014700731
Jason Hall <jason.hall3@cox.com>
2019-08-28 18:47:33 -0400

    templates from META tab are working.

--- day_three/main.py
+++ day_three/main.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import xlrd
 from pprint import pprint
+from jinja2 import Environment, FileSystemLoader
 
 xls_path = Path('./xls')
 xls_files = list(xls_path.glob('*.xlsx'))
@@ -47,6 +48,10 @@ def load_sheets(book, ignore_sheets=[]):
     parent_sheets = []
 
     for sheet_name in book.sheet_names():
+        if sheet_name in ignore_sheets:
+            print(f'Skipping sheet {sheet_name}')
+            continue
+
         if '-' in sheet_name:
             child_sheets.append(book.sheet_by_name(sheet_name))
         else:
@@ -56,8 +61,6 @@ def load_sheets(book, ignore_sheets=[]):
     # process the parent sheets first...
     # parents have to exist before they can have children
     for sheet in parent_sheets:
-        if sheet_name in ignore_sheets:
-            continue
 
         sheet_name = sheet.name.lower().strip().replace(' ','_')
         # collect the headers from the sheet
@@ -74,8 +77,6 @@ def load_sheets(book, ignore_sheets=[]):
             data_dict[sheet_name].append(record)
 
     for sheet in child_sheets:
-        if sheet_name in ignore_sheets:
-            continue
 
         headers = [str(cell.value).lower().strip().replace(' ', '_') for cell in sheet.row(0)]
         parent_key_col = -1
@@ -121,10 +122,49 @@ def load_sheets(book, ignore_sheets=[]):
             parent_pointer.append(record)
     return data_dict
 
+def load_meta(wb):
+    sheet = wb.sheet_by_name('META')
+
+    metadata = {}
+    # FIRST ROW IS HEADERS
+    for row_idx in range(1, sheet.nrows):
+        key = sheet.cell(row_idx, 0).value
+        value = sheet.cell(row_idx, 1).value.strip()
+
+        # ignore_sheets can be a comman separated list
+        if key == 'ignore_sheets' and ',' in value:
+            value = value.split(',')
+            value = [ item.strip() for item in value ]
+
+        # always strip bad data off the front and back of the line
+        metadata[key] = value
+
+    return metadata
+
+def render(template_dir, template, data):
+    env = Environment(loader=FileSystemLoader(template_dir))
+    template = env.get_template(template)
+
+    return template.render(data = data)
+
+
 
 if __name__ == '__main__':
     for file in xls_files:
         cur_book = open_file(file)
 
-        relational_wb_data = load_sheets(cur_book)
-        pprint(relational_wb_data)
+        if 'META' in cur_book.sheet_names():
+            meta = load_meta(cur_book)
+            documentation_template = meta['documentation_template_dir'] + meta['documentation_template']
+            pprint(meta)
+        else:
+            meta = {}
+            meta['ignore_sheets'] = []
+
+        relational_wb_data = load_sheets(cur_book, ignore_sheets=meta['ignore_sheets'])
+
+        # pprint(relational_wb_data)
+
+        if 'template_dir' in meta.keys():
+            xml = render(meta['template_dir'], meta['template'], relational_wb_data)
+            print(xml)
